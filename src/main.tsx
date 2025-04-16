ad53022a950174cbfc90e431ed31970bf63f0b44
AbelX18 <abelx8cam@gmail.com>
2025-04-16 07:43:07 -0300

    React Router Dom Update Latest version

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
 
-// Pajas
+// Páginas
 import Products from './pages/Products'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
@@ -17,35 +20,37 @@ import Staff from './pages/Staff'
 import ControlCategories from './pages/ControlCategories'
 import ControlStaff from './pages/ControlStaff'
 
-
 const queryClient = new QueryClient()
+
+const router = createBrowserRouter(
+  [
+    {
+      element: <EccomerceLayout />,
+      children: [
+        { path: '/products', element: <Products /> },
+        { path: '/login', element: <Login /> },
+        { path: '/register', element: <Signup /> },
+        { path: '/ticket', element: <Ticket /> },
+        { path: '/about', element: <About /> },
+      ],
+    },
+    {
+      element: <StaffLayout />,
+      children: [
+        { path: '/staff/main', element: <Staff /> },
+        { path: '/staff/products', element: <ControlProducts /> },
+        { path: '/staff/categories', element: <ControlCategories /> },
+        { path: '/staff/user', element: <ControlStaff /> },
+      ],
+    },
+  ],
+
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-          <Router>
-            <Routes>
-              <Route element={<EccomerceLayout/>}>
-                <Route path="/products" element={<Products />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Signup />} />
-                <Route path="/products" element={<Products />} />
-                <Route path="/ticket" element={<Ticket />} />
-                <Route path="/about" element={<About />} />
-              </Route>
-
-
-              <Route element={<StaffLayout/>}>
-                <Route path='/staff/main' element={<Staff/>}/>
-                <Route path="/staff/products" element={<ControlProducts/>}/>
-                <Route path="/staff/categories" element={<ControlCategories/>}/>
-                <Route path="/staff/user" element={<ControlStaff/>}/>
-              </Route>
-
-            </Routes>
-          </Router>
-
-
+      <RouterProvider router={router} />
     </QueryClientProvider>
-  </StrictMode>,
+  </StrictMode>
 )
