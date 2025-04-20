566ef909e35b180732d1fad969308aa197c87a66
LordGh0st <pablourojas@protonmail.com>
2025-04-20 18:05:01 -0300

    Updated styles for buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,29 +25,29 @@ const Header = () => {
           </div>
 
           <div className="flex items-center space-x-4">
-            <Link to="/ticket" className="">
+            <Link to="/ticket" className="btn-arkadia">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
               </svg>
             </Link>
             {isLoggedIn ? (
               <div className="relative group">
-                <button className="flex items-center space-x-2">
+                <button className="flex items-center space-x-2 btn-arkadia">
                   <span>Cuenta</span>
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                     <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
                   </svg>
                 </button>
                 <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 hidden group-hover:block">
-                  <Link to="/profile" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Perfil</Link>
-                  <Link to="/orders" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Ordenes</Link>
-                  <button className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Logout</button>
+                  <Link to="/profile" className="block btn-arkadia">Perfil</Link>
+                  <Link to="/orders" className="block btn-arkadia">Ordenes</Link>
+                  <button className="block btn-register">Logout</button>
                 </div>
               </div>
             ) : (
               <div className="flex space-x-4">
                 <Link to="/login" className="btn-arkadia">Login</Link>
-                <Link to="/register" className="bg-red-600 text-white px-4 py-2 rounded-md hover:bg-red-700">Registrarse</Link>
+                <Link to="/register" className="btn-register ">Registrarse</Link>
               </div>
             )}
           </div>
